51995b471fa6d8b45835f34203ad5883beea0090
Rhobbiem <robbinson.mu@gmail.com>
2022-12-13 08:21:01 -0800

    task 1. The last digit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z')
+	putchar(c);
+	c++;
+		
+	putchar('\n');
+	return (0);
+}
