e6dac7ee3d667c445969de07c4158a6115bc250e
Rhobbiem <robbinson.mu@gmail.com>
2022-12-13 12:09:10 -0800

    Task 6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int c;
+
+	c = 0;
+	while
+		(c < 10) {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
