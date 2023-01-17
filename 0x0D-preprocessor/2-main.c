90073b5c737e501d684a4bc9b559ae26a64673a7
Rhobbiem <robbinson.mu@gmail.com>
2023-01-17 12:16:19 -0800

    2. File name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *  * main - prints the name of the file it was compiled from
- *   *
- *    * @void: no parameters
- *     *
- *      * Return: 0 is success
- *       */
+ * main - prints the name of the file it was compiled from
+ *
+ * @void: no parameters
+ *
+ * Return: 0 is success
+ */
 int main(void)
 {
 		printf("%s\n", __FILE__);
